c61ed9500afa5e1232e429b6f6456e64285cabb6
Aditya-Patel24 <adityasp243@gmail.com>
2024-03-17 00:48:04 +0530

    fix

--- src/component/NewsItem.jsx
+++ src/component/NewsItem.jsx
@@ -5,24 +5,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card ">
-          <img
-            src={
-              imageurl
-                ? imageurl
-                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ0-82ELFvRIrelvLXTK7rKvja6eGcLn82vyqEO7Zpwg&s"
-            }
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={ imageurl ? imageurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ0-82ELFvRIrelvLXTK7rKvja6eGcLn82vyqEO7Zpwg&s"}className="card-img-top"alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a
-              href={urlId}
-              target="_blank"
-              rel="noreferrer"
-              className="btn btn-primary btn-sm"
-            >
+            <a href={urlId} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm" >
               Read More
             </a>
           </div>
