3a7d421260b0a8bd2a5b892fd3225f2aeb359819
Aditya-Patel24 <adityasp243@gmail.com>
2024-03-17 17:21:44 +0530

    Update NewsItem.jsx styling

--- src/component/NewsItem.jsx
+++ src/component/NewsItem.jsx
@@ -5,7 +5,7 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card ">
-        <span className="position-relative  center-100 translate-middle badge  bg-dark " style={{left:'50%',top:'10px'}}>{source} </span>
+        <span className="position-absolute center-100 translate-middle badge bg-dark " style={{left:'50%',top:'10px',width:"100%",borderBottomLeftRadius:"0px",borderBottomRightRadius:"0px "}}>{source} </span>
           <img src={ imageurl ? imageurl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ0-82ELFvRIrelvLXTK7rKvja6eGcLn82vyqEO7Zpwg&s"}className="card-img-top"alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
