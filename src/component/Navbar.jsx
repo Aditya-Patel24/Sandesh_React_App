0e80fd370a169f10331927738b6808af02cf4841
Aditya-Patel24 <adityasp243@gmail.com>
2024-03-18 17:54:43 +0530

    Update Navbar styling to use dark theme

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,7 +7,7 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className=" navbar navbar-expand-lg bg-body-tertiary  ">
+        <nav className=" navbar navbar-expand-lg navbar-dark bg-dark  ">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/business">
               Sandesh
