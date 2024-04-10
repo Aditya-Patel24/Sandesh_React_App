e7d268861ecc937ba9ab47bbf5224dc1ea485570
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-10 22:44:21 +0530

    length

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Sandesh_React_App/',
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
+  plugins: [react()]
 });
 
