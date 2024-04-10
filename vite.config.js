74df6ec8df1478e386bb1cf12c14b3f93281023c
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-10 22:09:06 +0530

    build

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sandesh_React_App/',
   plugins: [react()],
   build: {
-    outDir: 'build', // This should match the directory you're trying to deploy
+    outDir: 'build',
   },
-})
+});
 
