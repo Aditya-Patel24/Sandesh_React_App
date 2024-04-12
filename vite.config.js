7a59a5ef3a20d7fde5e95ca7273c87adcf319709
Aditya-Patel24 <adityasp243@gmail.com>
2024-04-12 15:31:00 +0530

    Update build configuration and remove unused files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Sandesh_React_App/',
   build: {
     outDir: 'build', // This should match the directory you're trying to deploy
   },
