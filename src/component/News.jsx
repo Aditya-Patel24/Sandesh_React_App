c2ba9f805ef093070849d506889f5e7b143d1655
Aditya-Patel24 <adityasp243@gmail.com>
2024-09-27 23:12:37 +0530

    chore: Update dependencies and fonts
    - Added axios dependency to package.json
    - Updated font-family in index.css and Navbar.css
    - Updated font-family and imageurl in NewsItem.jsx
    - Added font stylesheet link in index.html
    - Updated package-lock.json with new dependencies

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -3,6 +3,7 @@ import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
+import axios from "axios";
 const News = (props) => {
   const [articles, setArticles] = useState([]);
   const [page, setPage] = useState(1);
@@ -10,13 +11,13 @@ const News = (props) => {
   const [totalResults, setTotalResults] = useState(0);
   const [hasMore, setHasMore] = useState(true);
   const categories = {
-      business: '#000066',
-      technology: '#66ccff',
-      health: '#009900',
-      entertainment: '#ff3300',
-      sports: '#ff6600',
-      science: '#660066',
-      general: '#8a90ff',
+    business: "#000066",
+    technology: "#66ccff",
+    health: "#009900",
+    entertainment: "#ff3300",
+    sports: "#ff6600",
+    science: "#660066",
+    general: "#8a90ff",
   };
   // const categoriesdark = {
   //   business: "gray",
@@ -30,41 +31,32 @@ const News = (props) => {
   // };
 
   const newsStyle = {
-    backgroundColor: props.mode === 'dark' ? '#212529' : categories[props.category] || "white",
-    color: props.mode === 'dark' ? 'white' : 'black'
+    backgroundColor:
+      props.mode === "dark" ? "#212529" : categories[props.category] || "white",
+    color: props.mode === "dark" ? "white" : "black",
   };
-
   useEffect(() => {
-    document.title = `Sandesh - ${
-      props.category.charAt(0).toUpperCase() + props.category.slice(1)
-    }`;
     update();
   }, []);
-
   const update = async () => {
     try {
+      setLoading(true);
       props.setProgress(10);
 
-      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=39b492da28fa492995bea597ec322fbd&page=${page}&pageSize=${props.pageSize}`;
-      let response = await fetch(url,{headers: {
-        'Upgrade-Insecure-Requests': '1',
-        'Accept': 'application/json'
-      }});
+      let url = `https://newsapi.org/v2/top-headlines?category=${
+        props.category
+      }&apiKey=${import.meta.env.VITE_APP_KEY}&page=${page}`;
+      let response = await axios.get(url);
+      console.log("data", response.data); // Check the structure here
+
       props.setProgress(30);
-      
-      // Check if the response is ok
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+
+      if (response.data.status !== "ok") {
+        throw new Error(`API error: ${response.data.message}`);
       }
-      let parseData = await response.json();
-      props.setProgress(50);
-  
-      if (parseData.status !== "ok") {
-        throw new Error(`API error: ${parseData.message}`);
-      }
-  
-      setArticles(parseData.articles);
-      setTotalResults(parseData.totalResults);
+
+      setArticles(response.data.articles);
+      setTotalResults(response.data.totalResults);
       setLoading(false);
       props.setProgress(100);
     } catch (error) {
@@ -72,68 +64,83 @@ const News = (props) => {
       setLoading(false);
     }
   };
-  
+
   const fetchMoreData = async () => {
     try {
       setPage(page + 1);
-      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=39b492da28fa492995bea597ec322fbd&page=${page}&pageSize=${props.pageSize}`;
-      let response = await fetch(url);
-  
-      // Check if the response is ok
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
+      let url = `https://newsapi.org/v2/top-headlines?category=${
+        props.category
+      }&apiKey=${import.meta.env.VITE_APP_KEY}&page=${page + 1}`;
+
+      let response = await axios.get(url);
+      console.log("More data response:", response.data); // Check the structure here
+
+      if (response.data.status !== "ok") {
+        throw new Error(`API error: ${response.data.message}`);
       }
-  
-      let parseData = await response.json();
-  
-      // Handle API response error
-      if (parseData.status !== "ok") {
-        throw new Error(`API error: ${parseData.message}`);
-      }
-  
-      if (articles.length + parseData.articles.length >= parseData.totalResults) {
+
+      if (
+        articles.length + response.data.articles.length >=
+        response.data.totalResults
+      ) {
         setHasMore(false);
       }
-      setArticles(articles.concat(parseData.articles));
-      setTotalResults(parseData.totalResults);
+
+      setArticles((prevArticles) => [
+        ...prevArticles,
+        ...response.data.articles,
+      ]);
+      setTotalResults(response.data.totalResults);
     } catch (error) {
       console.error("Error fetching more news:", error);
     }
+    finally {
+      setLoading(false);
+      props.setProgress(100);
+    }
   };
-  
+
   return (
     <div style={newsStyle}>
-      <h1 className="text-center  pt-3" style={{marginTop:"55px", color: "rgba(255, 255, 255, 0.88)", textShadow:"2px 2px 2px rgba(0, 0, 0, 0.3), 0 0 25px rgba(255, 255, 255, 0.3), 0 0 5px rgba(255, 255, 255, 0.5)"}}>
+      <h1
+        className="text-center  pt-3"
+        style={{
+          marginTop: "55px",
+          color: "rgba(255, 255, 255, 0.88)",
+          textShadow:
+            "2px 2px 2px rgba(0, 0, 0, 0.3), 0 0 25px rgba(255, 255, 255, 0.3), 0 0 5px rgba(255, 255, 255, 0.5)",
+        }}
+      >
         Sandesh from{" "}
-        {props.category.charAt(0).toUpperCase() +
-          props.category.slice(1)}
+        {props.category.charAt(0).toUpperCase() + props.category.slice(1)}
       </h1>
-      {loading && <Spinner />}
+      {loading}
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
         hasMore={hasMore}
-        loader={<Spinner/>}
+        loader={<Spinner />}
       >
         <div className="container ">
           <div className="row">
-            {articles &&
-              articles.map((element, index) => {
-                return (
-                  <div key={element.url} className="col-md-4">
-                    <NewsItem
-                      title={element.title}
-                      description={element.description}
-                      imageurl={element.urlToImage}
-                      urlId={element.url}
-                      author={element.author}
-                      date={element.publishedAt}
-                      source={element.source.name}
-                      mode={props.mode}
-                    />
-                  </div>
-                );
-              })}
+            {articles.map((element) => {
+              return (
+                <div key={element.url} className="col-md-4">
+                  <NewsItem
+                    title={element.title || "No Title Available"}
+                    description={
+                      element.description || "No Description Available"
+                    }
+                    imageurl={element.urlToImage || "default-image-url.jpg"} // Add a fallback image if needed
+                    urlId={element.url}
+                    author={element.author || "Unknown"}
+                    date={element.publishedAt}
+                    source={element.source?.name || "Unknown Source"} // Safe access
+                    mode={props.mode}
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
       </InfiniteScroll>
